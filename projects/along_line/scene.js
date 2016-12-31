994208bcc286cb02ea7ee84f7e431ade90ef7f27
stintosestudios <stintose.studios@gmail.com>
2016-12-31 11:40:49 -0500

    draw box method

--- projects/along_line/scene.js
+++ projects/along_line/scene.js
@@ -29,36 +29,63 @@ scene({
             skin : {
                 appendRender : function (ctx) {
 
-                    var x,
-                    y,
-                    point,
-                    i = 0,
-                    totalPoints = 50,
-                    angle; // the angle for the given point
+                    var totalPoints = 50,
 
-                    var getPoint = function (i) {
+                    getPoint = function (i) {
 
                         var bias = Math.abs(.5 - this.percentDone) / .5;
 
                         return {
 
                             x : this.viewPort.w / totalPoints * i,
-                            y : this.viewPort.h - Math.pow(1.115, i) - (3 * i)
+                            y : this.viewPort.h - Math.pow(1.115, i) - (2 * i)
 
                         }
+                    },
+
+                    drawLine = function () {
+
+                        var i = 0,
+                        point;
+
+                        ctx.save();
+                        while (i < totalPoints) {
+
+                            ctx.strokeStyle = 'rgba(255,255,255,.5)';
+                            ctx.beginPath();
+                            point = getPoint.call(this, i - 1);
+                            ctx.moveTo(point.x, point.y);
+
+                            point = getPoint.call(this, i);
+                            ctx.lineTo(point.x, point.y);
+                            ctx.stroke();
+
+                            i += 1;
+
+                        }
+                        ctx.restore();
+
+                    },
+
+                    drawBox = function () {
+
+                        var i = Math.floor(totalPoints) * this.percentDone,
+                        point = getPoint.call(this, i),
+                        angle = Math.atan2(point.y, point.x);
+
+                        // draw box
+                        ctx.save();
+                        ctx.strokeStyle = '#00ffff';
+                        ctx.lineWidth = 3;
+                        ctx.translate(point.x - 64, point.y - 64);
+                        ctx.rotate(angle);
+                        ctx.strokeRect(0, 0, 128, 128);
+                        ctx.restore();
+
                     };
 
-                    i = Math.floor(totalPoints) * this.percentDone;
-                    point = getPoint.call(this, i);
-                    angle = Math.atan2(point.y, point.x);
-
-                    // draw
-                    ctx.save();
-                    ctx.strokeStyle = '#00ffff';
-                    ctx.translate(point.x - 64, point.y - 64);
-                    ctx.rotate(angle);
-                    ctx.strokeRect(0, 0, 128, 128);
-                    ctx.restore();
+                    drawLine.call(this);
+                    drawBox.call(this);
 
                 }
             }
